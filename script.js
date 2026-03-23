d05462e2b39d7c2c81d0fd49d67118b6a5013537
Youssef0356 <nejiyoussef081@gmail.com>
2026-03-23 14:38:21 +0100

    added the buttons logic

--- script.js
+++ script.js
@@ -7,11 +7,28 @@ loadTrack()
 
 })
 
-//we load the track now by grapping ids
+//we load the track now by grapping ids and use the ready functions that js provided
 
 let audio = document.getElementById('audio')
 let title = document.getElementById('title')
 function loadTrack(){
     audio.src = tracks[index].src
     title.textContent = tracks[index].title
+}
+
+
+document.getElementById('play').onclick = () => {
+    if (audio.paused) audio.onplay()
+        else audio.pause()
+
+}
+document.getElementById('next').onclick = () => {
+    index = (index+1) % tracks.length
+    loadTrack()
+    audio.onplay()
+}
+document.getElementById('prev').onclick = () => {
+    index = (index-1) % tracks.length
+    loadTrack()
+    audio.onplay()
 }
